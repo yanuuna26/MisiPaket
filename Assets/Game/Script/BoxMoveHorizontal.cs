a1b190084df470586d86652dc9fd9ba0b95686da
Yanuar Setyoningsih <yanuar.setyoningsih@gmail.com>
2025-11-17 10:39:37 +0700

    Box, tembok, tanah

---
+++ Assets/Game/Script/BoxMoveHorizontal.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class BoxMoveHorizontal : MonoBehaviour
+{
+    public float kecepatan = 3f;
+    public bool isMoveRight = true;
+    public bool isDropped = false;
+
+    void Update()
+    {
+        if (!isDropped)
+        {
+            float dir = isMoveRight ? 1f : -1f;
+            transform.Translate(Vector3.right * dir * kecepatan * Time.deltaTime);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        //Debug.Log("Tabrakan dengan: " + col.collider.name);
+
+        if (col.collider.CompareTag("tembok"))
+        {
+            isMoveRight = !isMoveRight;
+        }
+        if (col.collider.CompareTag("lantai"))
+        {
+            isDropped = true;
+        }
+    }
+}
---
+++ Assets/Game/Script/BoxSpawner.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using UnityEngine;
+
+public class BoxSpawner : MonoBehaviour
+{
+
+    public GameObject boxPrefab;
+    public GameObject currentBox;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        SpawnNewBox();
+        // MoveHorizontal();
+        StartCoroutine(waktuSpawnBox());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DetectInput();
+    }
+
+    void SpawnNewBox()
+    {
+        Vector3 pos = Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0.90f, 10f));
+
+        currentBox = Instantiate(boxPrefab, pos, Quaternion.identity);
+
+        // Reset status movement dan fisika box
+        BoxMoveHorizontal bm = currentBox.GetComponent<BoxMoveHorizontal>();
+        bm.isDropped = false;
+        bm.isMoveRight = true;
+
+        Rigidbody2D rb = currentBox.GetComponent<Rigidbody2D>();
+        rb.gravityScale = 0;
+    }
+
+
+    void DetectInput()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            DropBox();
+        }
+    }
+
+    void DropBox()
+    {
+        BoxMoveHorizontal bm = currentBox.GetComponent<BoxMoveHorizontal>();
+        bm.isDropped = true;
+        Rigidbody2D rb = currentBox.GetComponent<Rigidbody2D>();
+        rb.gravityScale = 3f;
+    }
+
+    void UpdateSpeed(int score) { }
+
+    IEnumerator waktuSpawnBox()
+    {
+        BoxMoveHorizontal bm = currentBox.GetComponent<BoxMoveHorizontal>();
+        if (bm.isDropped)
+        {
+            yield return new WaitForSeconds(3);
+            SpawnNewBox();
+        }
+        yield return null;
+    }
+
+}
