c8bc494936c1e578cd63e9d2efcbc77fb6350181
Faishal Ahmad Rafi <farmoron@gmail.com>
2025-11-18 09:01:26 +0700

    MainMenuDone

--- Assets/Game/Script/MainMenu.cs
+++ Assets/Game/Script/MainMenu.cs
@@ -26,16 +26,11 @@ public class MainMenu : MonoBehaviour
     public void exitCredit()
     {
         SceneManager.LoadScene("MainMenu");
-    }   
+    }
+    public void BackMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
