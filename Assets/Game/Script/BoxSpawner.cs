b0ab875858dfac4c26b12587f952a23505f35406
Faishal Ahmad Rafi <farmoron@gmail.com>
2025-11-18 13:52:34 +0700

    Polishing button and highscore

--- Assets/Game/Script/BoxSpawner.cs
+++ Assets/Game/Script/BoxSpawner.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using JetBrains.Annotations;
 using TMPro;
 using UnityEngine;
 
@@ -7,37 +8,66 @@ public class BoxSpawner : MonoBehaviour
 
     public GameObject boxPrefab;
     public GameObject currentBox;
-    public GameObject onoff;
+    public GameObject gameoverimg;
+    public GameObject RestartButton;
+    public GameObject ExitButton;
     public TMP_Text skorText;
+    public TMP_Text HiskorText;
 
     public AudioSource suaraPaket;
+    public AudioSource backsoundMusic;
+    public AudioSource dropBoxSound;
+    public AudioSource gameOverSound;
 
     public float kecepatan = 0f;
+    public int highscore = 0;
     public int skor = 0;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        Time.timeScale = 1f;
         SpawnNewBox();
-        // MoveHorizontal();
+        if(suaraPaket != null)
+        {
+            suaraPaket.Play();
+        }
+        //MoveHorizontal();
         //StartCoroutine(waktuSpawnBox());
+        if (PlayerPrefs.HasKey("highscore"))
+        {
+            highscore = PlayerPrefs.GetInt("highscore");
+            HiskorText.text = "High Score : " + highscore.ToString();
+        }
+        if(backsoundMusic != null)
+        {
+            backsoundMusic.Play();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         DetectInput();
+        
     }
-
+    public void SimpanHighScore()
+    {
+        if  (skor > highscore)
+        {
+        highscore = skor;
+        PlayerPrefs.SetInt("highscore", highscore);
+        PlayerPrefs.Save();
+        HiskorText.text = "High Score : " + highscore.ToString();
+        }
+        //PlayerPrefs.DeleteKey("highscore");
+    }
     void SpawnNewBox()
     {
         Vector3 pos = Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0.90f, 10f));
 
         currentBox = Instantiate(boxPrefab, pos, Quaternion.identity);
-        if(suaraPaket != null)
-        {
-            suaraPaket.Play();
-        }
+        
 
         // Reset status movement dan fisika box
         BoxMoveHorizontal bm = currentBox.GetComponent<BoxMoveHorizontal>();
